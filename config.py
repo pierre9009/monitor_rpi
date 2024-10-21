5af07581a1421638fe5cc79bfec2fdb221e4b404
Pierre-Minh TRAN <pierre.minh9009@gmail.com>
2024-10-21 16:56:35 +0000

    modif config

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 #config.py
 SLAVES = [
-    "http://78.194.193.118:5000/resources", #Paris
-    "http://192.168.1.102:5000/resources",
-    "http://192.168.1.103:5000/resources",
-    "http://192.168.1.104:5000/resources"
+    "http://rp-paris.pierreminh.com:5000/resources", #Paris
+    "http://rp-faucon.pierreminh.com:5000/resources",
+    "http://rp-vitry.pierreminh.com:5000/resources"
 ]
